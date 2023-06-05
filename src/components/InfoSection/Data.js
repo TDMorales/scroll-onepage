be4198409393add87c002d779536947fc6c338d1
Timothy Morales <timothydanielmorales@gmail.com>
2023-06-04 23:39:52 -0500

    progress

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,18 @@
+import svg1 from '../../assets/images/svg-1.svg'
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: "Premium Bank",
+    headline: "Unlimited transactions with zero fees",
+    description: "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+    buttonLabel: "Get Started",
+    imgStart: false,
+    img: svg1,
+    alt: "Car",
+    dark: true,
+    primary: true,
+    darkText: false
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Sidebar from '../components/Sidebar/Sidebar'
 import NavBar from '../components/NavBar/NavBar'
 import HeroSection from '../components/HeroSection/HeroSection'
 import InfoSection from '../components/InfoSection/InfoSection'
+import { homeObjOne } from '../components/InfoSection/Data'
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -17,7 +18,7 @@ const Home = () => {
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <NavBar toggle={toggle} />
             <HeroSection />
-            <InfoSection />
+            <InfoSection {...homeObjOne} />
         </>
     )
 }
